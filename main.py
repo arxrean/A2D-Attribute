5b9bae1a98db9e3c26b01ecf7f810a3942c69fd3
arxrean <602052254@qq.com>
2019-07-20 14:43:01 +0800

    abc

--- main.py
+++ main.py
@@ -1,2 +1,39 @@
-if __name__=='__main__':
-	pass
+import os
+import argparse
+from torch.utils.data import Dataset, DataLoader
+from torchvision import datasets, transforms
+import torch.optim as optim
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+from torch import Tensor
+
+
+def p_parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--session_name", default="a2d", type=str)
+    # data
+    parser.add_argument(
+        "--a2d_root", default='/', type=str)
+    # config
+    parser.add_argument("--num_workers", default=8, type=int)
+    parser.add_argument("--batch_size", default=16, type=int)
+    parser.add_argument("--max_epoches", default=20, type=int)
+    # train
+    parser.add_argument("--lr", default=0.1, type=float)
+    parser.add_argument("--wt_dec", default=5e-4, type=float)
+    # save
+    parser.add_argument("--save_weights", default='save/weights', type=str)
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    transform_dict = {'train': None, 'val': None}
+
+
+if __name__ == '__main__':
+    global args
+    args = p_parse()
