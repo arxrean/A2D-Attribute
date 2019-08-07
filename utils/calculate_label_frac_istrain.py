b6faaf03e436747a3ea3f4c7470673af88243d51
CQUSlash <CQUSlash@users.noreply.github.com>
2019-08-07 16:01:47 +0800

    Update calculate_label_frac_istrain.py

--- utils/calculate_label_frac_istrain.py
+++ utils/calculate_label_frac_istrain.py
@@ -12,41 +12,30 @@ def joint_frac_istrain(embed_size=43):
              67: 33, 68: 34, 69: 35, 72: 36, 73: 37, 75: 38, 76: 39, 77: 40,
              78: 41, 79: 42}
     csv = pd.read_csv('./repo/newVideoSet.csv')
+    csv = csv[csv['usage'] == 0]
     num_items = len(csv)
+    csv = csv.values
 
-    print(1)
-    #print(type(csv.loc[0]['is_train']))
-
-    for i in range(num_items):
-        item = csv.loc[i]
-        if item['is_train'] == 0:
-            csv.drop([i],inplace=True) 
-    
-    num_items = len(csv)
-    print(num_items)
-
-    for i in range(num_items):
-        item = csv.loc[i]
-        listlabel = item['label'].split(' ')
+    for item in csv:
+        listlabel = item[1].split(' ')
         labels = []
         for label in listlabel:
             label = int(float(label))
             if label in valid:
                 labels.append(valid[label])
-            else:
-                print('label:%d in csv[%d] is not in valid'%(label, i))
         labels = to_categorical(labels, num_classes=embed_size)
         labels = labels.sum(axis=0)
         res += labels
     print(res)
     N = np.ones(embed_size) * num_items
+    print(num_items)
     res = res / (N-res)
     print(res)
 
     #np.save('./repo/joint_label_frac_istrain.npy', res)
 
     return res
-    
+ 
 if __name__ == '__main__':
-    #res = joint_frac_istrain()
-    joint_frac_istrain()
+    res = joint_frac_istrain()
+    #joint_frac_istrain()
