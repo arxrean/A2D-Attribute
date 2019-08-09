dcf5a1678255f643f82c857e6ff06b706c2dc3a5
CQUSlash <CQUSlash@users.noreply.github.com>
2019-08-09 19:40:51 +0800

    Update calculate_label_frac.py

--- utils/calculate_label_frac.py
+++ utils/calculate_label_frac.py
@@ -37,6 +37,56 @@ def joint_frac(embed_size=43):
     np.save('./repo/joint_label_frac.npy', res)
 
     return res
+ 
+
+def actor_action_frac(actor_embed_size=7,action_embed_size=9):
+    actor_res = np.zeros(actor_embed_size)
+    action_res = np.zeros(action_embed_size)
+
+    actor_valid = {1: 0, 2: 1, 3: 2, 4: 3, 5: 4, 6: 5, 7: 6}
+    action_valid = {1: 0, 2: 1, 3: 2, 4: 3, 5: 4, 6: 5, 7: 6, 8: 7, 9: 8}
     
+    csv = pd.read_csv('./repo/newVideoSet.csv')
+    num_items = len(csv)
+
+    for i in range(num_items):
+        item = csv.loc[i]
+        listlabel = item['label'].split(' ')
+        actor_label = []
+        action_label = []
+        for label in listlabel:
+            label = int(float(label))
+            actor = label // 10
+            action = label % 10
+            if actor in actor_valid:
+                actor_label.append(actor_valid[actor])
+            else:
+                print('actor_label:%d in csv[%d] is not in actor_valid'%(actor, i))
+            if action in action_valid:
+                action_label.append(action_valid[action])
+            else:
+                print('action_label:%d in csv[%d] is not in action_valid'%(action, i))
+            
+        actor_label = to_categorical(actor_label, num_classes=actor_embed_size)
+        actor_label = actor_label.sum(axis=0)
+        actor_res += actor_label
+
+        action_label = to_categorical(action_label, num_classes=action_embed_size)
+        action_label = action_label.sum(axis=0)
+        action_res += action_label
+    print('actor_res:', actor_res)
+    print('action_res:', action_res)
+
+    N_actor = np.ones(actor_embed_size) * num_items
+    actor_res = actor_res / (N_actor - actor_res)
+
+    N_action = np.ones(action_embed_size) * num_items
+    action_res = action_res / (N_action - action_res)
+    print('actor_res:', actor_res)
+    print('action_res:', action_res)
+
+    return actor_res, action_res
+
 if __name__ == '__main__':
     res = joint_frac()
+    actor_res, action_res = actor_action_frac()
