cd11d3b450f635083ffaa1e88b7ed43fe4808520
arxrean <602052254@qq.com>
2019-09-07 03:19:07 +0800

    abc

--- extract_frames.py
+++ extract_frames.py
@@ -3,16 +3,16 @@ from subprocess import check_call
 
 def worker(video_f):
     video_name = video_f.split('.')[0]
-    folder = os.path.join('/mnt/lustre/jiangsu/dlar/home/zyk17/data/A2D/Release/pngs320H', video_name)
+    folder = os.path.join('/u/zkou2/Data/A2D/Release/pngs320H', video_name)
     if not os.path.exists(folder):
         os.mkdir(folder)
-    frame_path = os.path.join('/mnt/lustre/jiangsu/dlar/home/zyk17/data/A2D/Release/pngs320H', video_name, '%05d.png')
-    result = check_call(['ffmpeg', '-i', os.path.join('/mnt/lustre/jiangsu/dlar/home/zyk17/data/A2D/Release/clips320H', video_f), frame_path])
+    frame_path = os.path.join('/u/zkou2/Data/A2D/Release/pngs320H', video_name, '%05d.png')
+    result = check_call(['ffmpeg', '-i', os.path.join('/u/zkou2/Data/A2D/Release/clips320H', video_f), frame_path])
     print(result)
 
 if __name__ == "__main__":
-    if not os.path.exists('/mnt/lustre/jiangsu/dlar/home/zyk17/data/A2D/Release/pngs320H'):
-        os.mkdir('/mnt/lustre/jiangsu/dlar/home/zyk17/data/A2D/Release/pngs320H')
-    lst = os.listdir('/mnt/lustre/jiangsu/dlar/home/zyk17/data/A2D/Release/clips320H')
+    if not os.path.exists('/u/zkou2/Data/A2D/Release/pngs320H'):
+        os.mkdir('/u/zkou2/Data/A2D/Release/pngs320H')
+    lst = os.listdir('/u/zkou2/Data/A2D/Release/clips320H')
     for vid in lst:
         worker(vid)
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import argparse
 from torch.utils.data import Dataset, DataLoader
 from torchvision import datasets, transforms
 import torch.optim as optim
@@ -15,37 +14,6 @@ from loader.A2DClsLoader import A2DClassification, A2DClassificationWithActorAct
 from model.net import getJointClassifier, getSplitClassifier
 from utils.opt import get_finetune_optimizer
 from utils.helper import get_pos_weight, bce_weight_loss
-
-
-def p_parse():
-	parser = argparse.ArgumentParser()
-	parser.add_argument("--session_name", default="a2d", type=str)
-	# data
-	parser.add_argument(
-		"--a2d_root", default='/mnt/lustre/jiangsu/dlar/home/zyk17/data/A2D/Release', type=str)
-	parser.add_argument('--csv_path', default='./repo/newVideoSet.csv')
-	parser.add_argument('--t', type=int, default=0)
-	parser.add_argument('--input_size', type=int, default=224)
-	parser.add_argument('--class_num', type=int, default=43)
-	parser.add_argument('--actor_num', type=int, default=7)
-	parser.add_argument('--action_num', type=int, default=9)
-	# config
-	parser.add_argument("--num_workers", default=8, type=int)
-	parser.add_argument("--batch_size", default=48, type=int)
-	parser.add_argument("--max_epoches", default=1000, type=int)
-	parser.add_argument("--cuda", default=False, type=bool)
-	parser.add_argument("--pretrained", default=True, type=bool)
-	# train
-	parser.add_argument("--lr", default=1e-5, type=float)
-	parser.add_argument("--lr_dec", default=1, type=float)
-	parser.add_argument("--wt_dec", default=5e-4, type=float)
-	# save
-	parser.add_argument("--save_root", default='./save/', type=str)
-
-	args = parser.parse_args()
-
-	return args
-
 
 def joint_classification(args):
 	train_transform = transforms.Compose([
