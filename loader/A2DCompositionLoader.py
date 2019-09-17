bd7b8ff9bb614acfe2025a7dd63a42aa61304a62
CQUSlash <CQUSlash@users.noreply.github.com>
2019-09-18 01:25:33 +0800

    Update A2DCompositionLoader.py

--- loader/A2DCompositionLoader.py
+++ loader/A2DCompositionLoader.py
@@ -16,6 +16,8 @@ class A2DComposition(tdata.Dataset):
         self.mode = mode
         self.csv = pd.read_csv(args.csv_path)
 
+        self.maxlenlabel, self.train_label_len_probability = self.train_sample_probability()
+
         # pdb.set_trace()
 
         self.actors_dict = {1: 'adult', 2: 'baby',
@@ -169,14 +171,48 @@ class A2DComposition(tdata.Dataset):
         ##################
 
         return train_data, test_data
+    
+    def train_sample_probability(self):
+        tr_labels = self.csv[self.csv['usage'] == 0]['label'].values
+        train_sample_num = len(tr_labels)
+        maxlenlabel = 0
+        for label in tr_labels:
+            if len(label.split(' ')) > maxlenlabel:
+                maxlenlabel = len(label.split(' '))
+        
+        train_label_len_probability = np.zeros(maxlenlabel)
+        for label in tr_labels:
+            train_label_len_probability[len(label.split(' ')) - 1] += 1
+        train_label_len_probability = (np.array(train_label_len_probability) / train_sample_num).tolist()
+
+        return maxlenlabel, train_label_len_probability
 
     def sample_negative(self, actions, actors):
+        sample_num = np.random.choice(self.maxlenlabel, p=self.train_label_len_probability) + 1
+        choice_index_list = np.random.choice(len(self.train_pairs), sample_num)
+        
+        samples = []
+        pos_pairs = []
+        
+        for i in range(len(actors)):
+            pos_pairs.append(actors[i] + ' ' + actions[i])
+
+        for index in choice_index_list:
+            samples.append(self.train_pairs[index])
+        
+        if sorted(samples) == sorted(pos_pairs):
+            return self.sample_negative(actions, actors)
+        else:
+            return [self.pair2idx[sample] for sample in samples]
+
+        '''
         sample = self.train_pairs[np.random.choice(
             len(self.train_pairs))]
         new_actor, new_action = sample.split(' ')[0], sample.split(' ')[1]
         if new_action in actions and new_actor in actors:
             return self.sample_negative(actions, actors)
         return self.action2idx[new_action], self.actor2idx[new_actor]
+        '''
 
     def idx2hot(self, idx_list, class_num):
         label = to_categorical(idx_list, num_classes=class_num)
@@ -194,9 +230,12 @@ class A2DComposition(tdata.Dataset):
             data = [img, self.idx2hot([self.pair2idx['{} {}'.format(
                 actors[i], actions[i])] for i in range(len(actors))], class_num=len(self.pairs))]
             if self.mode == 'train':
-                neg_action, neg_actor = self.sample_negative(actions, actors)
-                data += [self.idx2hot([neg_action], class_num=len(self.actions)),
-                         self.idx2hot([neg_actor], class_num=len(self.actors))]
+                neg_pairs = self.sample_negative(actions, actors)
+                data += [self.idx2hot(neg_pairs, class_num=len(self.pairs))]
+                '''
+                data += [self.idx2hot(neg_actions, class_num=len(self.actions)),
+                         self.idx2hot(neg_actors, class_num=len(self.actors))]
+                '''
 
             # pdb.set_trace()
             if self.activations is not None:
