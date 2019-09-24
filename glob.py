666f2f01102b830484af00e687294cc182477f68
CQUSlash <CQUSlash@users.noreply.github.com>
2019-09-25 00:42:46 +0800

    Update glob.py

--- glob.py
+++ glob.py
@@ -5,8 +5,8 @@ def p_parse():
 	parser.add_argument("--session_name", default="a2d", type=str)
 	# data
 	parser.add_argument(
-		# "--a2d_root", default='E:/A2D/Release', type=str)
-		"--a2d_root", default='/u/zkou2/Data/A2D/Release', type=str)
+		"--a2d_root", default='E:/A2D/Release', type=str)
+		#"--a2d_root", default='/u/zkou2/Data/A2D/Release', type=str)
 	parser.add_argument('--csv_path', default='./repo/newVideoSet.csv')
 	parser.add_argument('--t', type=int, default=0)
 	parser.add_argument('--input_size', type=int, default=224)
@@ -14,8 +14,8 @@ def p_parse():
 	parser.add_argument('--actor_num', type=int, default=7)
 	parser.add_argument('--action_num', type=int, default=9)
 	# config
-	parser.add_argument("--num_workers", default=8, type=int)
-	parser.add_argument("--batch_size", default=512, type=int)
+	parser.add_argument("--num_workers", default=0, type=int)
+	parser.add_argument("--batch_size", default=400, type=int)
 	parser.add_argument("--max_epoches", default=1000, type=int)
 	parser.add_argument("--cuda", default=False, type=bool)
 	parser.add_argument("--pretrained", default=True, type=bool)
@@ -26,7 +26,7 @@ def p_parse():
 	parser.add_argument("--wt_dec", default=5e-4, type=float)
 	parser.add_argument("--op_img_dim", default=300, type=int)
 	parser.add_argument("--triplet_margin", default=1.0, type=float)
-	parser.add_argument("--constraint_cls", default=1.0, type=float)
+	parser.add_argument("--constraint_cls", default=10, type=float)#10 100 1000
 	# save
 	parser.add_argument("--save_root", default='./save/', type=str)
 	# feature
